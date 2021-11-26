b21d929c5849d2f53afa0300e77c450aa7c08506
shashank-indukuri <shashank.indukuri05@gmail.com>
2021-11-25 23:33:10 -0500

    Deploy to Heroku

--- app.js
+++ app.js
@@ -15,10 +15,14 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "public")));
-
 app.use("/auth", authRouter);
 app.use("/users", usersRouter);
 app.use("/todos", todosRouter);
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 module.exports = app;
